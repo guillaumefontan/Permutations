c4a231fc2ed90498d7d58b953e3b0cb36aa450b6
guillaumefontan <82604059+guillaumefontan@users.noreply.github.com>
2021-04-15 17:40:43 +0100

    Add files via upload

---
+++ Permutations/IPermutations.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Permutations
+{
+    /// <summary>
+    /// Interface for the Permutations class.
+    /// </summary>
+    public interface IPermutations
+    {
+        /// <summary>
+        /// Calls Input and HeapPermutation.
+        /// </summary>
+        public Task Run();
+    }
+}
